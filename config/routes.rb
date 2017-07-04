b5d4bd7cf2b7c7bb9d8935cbeea17f73716eb766
Vitalij Bliferniz <vitalij.bliferniz@web.de>
2017-07-04 19:57:16 +0200

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :books do
+    resources :notes, only: [:create, :destroy]
+  end
+  root to: "books#index"
+end
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.destroy_all
+
+Book.create! [
+	{ name: "Eloquent Ruby", author: "Russ Olsen"},
+	{ name: "Beginning Ruby", author: "Peter Cooper"},
+	{ name: "Metaprogramming Ruby 2", author: "Paolo Perrotta"},
+	{ name: "Design Patterns in Ruby", author: "Russ Olsen"},
+	{ name: "The Ruby Programming Language", author: "David Flanagan"}
+]
